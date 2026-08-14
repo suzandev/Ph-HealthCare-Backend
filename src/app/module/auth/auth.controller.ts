f3fa9a38a39c66bbd50aa1878258571fc5ed2d95
Suzan Chandra <suzanchandra@Suzans-Mac-mini.local>
2026-08-14 16:08:23 +0600

    feat: implement validation middleware and schemas for user registration and login

--- src/app/module/auth/auth.controller.ts
+++ src/app/module/auth/auth.controller.ts
@@ -4,27 +4,10 @@ import { catchAsync } from "../../utils/catchAsync";
 import { sendResponse } from "../../utils/sendResponse";
 import type { IRequestUser } from "./auth.interface";
 import { AuthService } from "./auth.service";
-import z from "zod";
-
-const PatientRegisterActionZodSchema = z.object({
-  name: z.string(),
-  email: z.email(),
-  password: z.string().min(8),
-  patient: z
-    .object({
-      contactNumber: z.string().optional(),
-    })
-    .optional(),
-});
 
 const registerPatient = catchAsync(async (req: Request, res: Response) => {
-  const payload = PatientRegisterActionZodSchema.safeParse(req.body);
-
-  if (!payload.success) {
-    throw new Error(payload.error.message);
-  }
-
-  const result = await AuthService.registerPatient(payload.data as any);
+  const payload = req.body;
+  const result = await AuthService.registerPatient(payload);
 
   const { accessToken, refreshToken, user, patient } = result;
 
---
+++ src/app/module/auth/auth.validation.ts
@@ -0,0 +1,48 @@
+import z from "zod";
+
+const PatientRegisterActionZodSchema = z.object({
+  name: z.string().min(3, "Name must at least 3 characters long!!!"),
+  email: z.email(),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters long" })
+    .max(30, { message: "Password cannot exceed 30 characters" })
+    .regex(/[A-Z]/, {
+      message: "Password must contain at least one uppercase letter",
+    })
+    .regex(/[a-z]/, {
+      message: "Password must contain at least one lowercase letter",
+    })
+    .regex(/[0-9]/, { message: "Password must contain at least one number" })
+    .regex(/[^A-Za-z0-9]/, {
+      message: "Password must contain at least one special character",
+    }),
+  patient: z
+    .object({
+      contactNumber: z.string().optional(),
+    })
+    .optional(),
+});
+
+const LoginZodSchema = z.object({
+  email: z.email(),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters long" })
+    .max(30, { message: "Password cannot exceed 30 characters" })
+    .regex(/[A-Z]/, {
+      message: "Password must contain at least one uppercase letter",
+    })
+    .regex(/[a-z]/, {
+      message: "Password must contain at least one lowercase letter",
+    })
+    .regex(/[0-9]/, { message: "Password must contain at least one number" })
+    .regex(/[^A-Za-z0-9]/, {
+      message: "Password must contain at least one special character",
+    }),
+});
+
+export const UserValidation = {
+  PatientRegisterActionZodSchema,
+  LoginZodSchema,
+};
